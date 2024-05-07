7732391447d7be22f902e67fe4b1132c4843b36c
atifibrahim7 <i221249@nu.edu.pk>
2024-05-07 12:05:33 -0400

    updated version

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int pacman_x = CELL_SIZE + 25/8;
 int pacman_y = CELL_SIZE + 25/4;
 
 int gameMapSkeleton[ROWS][COLS] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
         {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
         {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
         {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
@@ -29,26 +29,26 @@ int gameMapSkeleton[ROWS][COLS] = {
         {1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1},
         {1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1},
         {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
-         {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-         {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, -1, -1, -1, -1, 0, 0, -1, -1, -1, -1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-         {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
-         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-         {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
-         {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-         {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
-         {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
-         {1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1},
-         {1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
-         {1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
-         {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
-         {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
-         {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
-         {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, -1, -1, -1, -1, 0, 0, -1, -1, -1, -1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+        {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1},
+        {1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
+        {1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+        {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 };
 int gameMap[ROWS][COLS] = {0};
 int totalScorePallet;
@@ -70,6 +70,9 @@ struct GhostData {
 
 // Mutex to protect user input
 pthread_mutex_t SharedmemMutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Mutex on gameBoard
+pthread_mutex_t GameBoardMutex = PTHREAD_MUTEX_INITIALIZER;
 
 // function that puts score pallets in gamegrid
 void intitializeGrid()
@@ -113,6 +116,8 @@ void drawGrid(sf::RenderWindow& window)
         for (int j = 0; j < COLS; j++)
         {
             // Draw pellets, power-ups, and walls
+            // lock shared gameBoard mutrx
+            pthread_mutex_lock(&GameBoardMutex);
             switch (gameMap[i][j])
             {
                 case 2:
@@ -138,6 +143,10 @@ void drawGrid(sf::RenderWindow& window)
                     break;
                
             }
+            // unlock mutex
+            pthread_mutex_unlock(&GameBoardMutex);
+
+
         }
     }
 }
@@ -155,7 +164,7 @@ void* ghostController(void* arg) {
         int directionY = 0;
         
         bool chosen = false;
-        
+        pthread_mutex_lock(&GameBoardMutex);
         if (ghost->direction == 1 || ghost->direction == 2) { // Up or down
             if (rand() % 2 == 0) { // Randomly choose left or right
                 if (gameMap[ghost->y / CELL_SIZE][(ghost->x + CELL_SIZE) / CELL_SIZE] != 1 &&
@@ -193,7 +202,7 @@ void* ghostController(void* arg) {
                 }
             }
         }
-        
+        pthread_mutex_unlock(&GameBoardMutex);
         if (!chosen) {
             // Continue in current direction
             if (ghost->direction == 1) {
@@ -211,18 +220,20 @@ void* ghostController(void* arg) {
         int nextY = ghost->y + directionY * CELL_SIZE;
         
         // Check for collisions with walls
+        pthread_mutex_lock(&GameBoardMutex);
         if (nextX >= 0 && nextX < 30 * CELL_SIZE && nextY >= 0 && nextY < 30 * CELL_SIZE &&
             (gameMap[nextY / CELL_SIZE][nextX / CELL_SIZE] == 0 || gameMap[nextY / CELL_SIZE][nextX / CELL_SIZE] == 2)) {
             ghost->x += directionX * CELL_SIZE;
             ghost->y += directionY * CELL_SIZE;
         }
+        pthread_mutex_unlock(&GameBoardMutex);
 
         //if close
-        // Sleep for a short duration to control the speed of the ghost
         if(closwindow == 1)
             {
                 pthread_exit(NULL);
             }
+        // Sleep for a short duration to control the speed of the ghost
         usleep(200000); // Adjust the sleep duration as needed
     }
     pthread_exit(NULL);
@@ -307,6 +318,7 @@ void movePacman()
     // boundary detection
     int nextX = pacman_x + pacman_direction_x * CELL_SIZE;
     int nextY = pacman_y + pacman_direction_y * CELL_SIZE;
+    pthread_mutex_lock(&GameBoardMutex);
     if (abs(gameMap[nextY / CELL_SIZE][nextX / CELL_SIZE]) == 1 || gameMap[nextY / CELL_SIZE][nextX / CELL_SIZE] == -1 || gameMap[nextY / CELL_SIZE][nextX / CELL_SIZE] == -2) {
            // cout << "Wall detected!" << endl;
             // set to the old direction
@@ -325,8 +337,9 @@ void movePacman()
         gameMap[nextY / CELL_SIZE][nextX / CELL_SIZE] = 0;
         totalScorePallet--;
         Score++;
-        cout<<Score<<endl;
+        //cout<<Score<<endl;
     } 
+    pthread_mutex_unlock(&GameBoardMutex);
 }
 
 int main() {
@@ -340,23 +353,33 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML window");
     // Create the yellow circle (player)
     sf::CircleShape pacman_shape(25/2);
-    sf::CircleShape ghost_shape(25/2);
+    sf::CircleShape ghost_shape1(25/2);
+    sf::CircleShape ghost_shape2(25/2);
+
     pacman_shape.setFillColor(sf::Color::Yellow);
-    ghost_shape.setFillColor(sf::Color::Red);
+    ghost_shape1.setFillColor(sf::Color::Red);
+    ghost_shape2.setFillColor(sf::Color::Blue);
     pacman_shape.setPosition(25/8, 25/4); // Set initial position to (100, 50)
     // set ghost position to (10, 10)
-    ghost_shape.setPosition(35, 38);
+    ghost_shape1.setPosition(35, 38);
+    ghost_shape2.setPosition(65, 38);
     // Create thread for user input
     pthread_t userInputThread;
     pthread_create(&userInputThread, nullptr, userInput, &window);
 
     // Initialize ghosts' data
     GhostData ghost1 = {35, 38}; // Example initial position
+    GhostData ghost2 = {899, 38}; // Example initial position
+    ghost2.direction = 3;
+
     // Initialize other ghost data as needed
 
     // Create threads for ghost controllers
     pthread_t ghost1Thread;
+    pthread_t ghost2Thread;
+
     pthread_create(&ghost1Thread, nullptr, ghostController, &ghost1);
+    pthread_create(&ghost2Thread, nullptr, ghostController, &ghost2);
     // Main loop
     while (window.isOpen()) 
     {
@@ -366,16 +389,18 @@ int main() {
         drawGrid(window);
         //moveGhost(ghost1);
         pacman_shape.setPosition(pacman_x, pacman_y); // Update pacman position
-        ghost_shape.setPosition(ghost1.x, ghost1.y); // Update ghost position
+        ghost_shape1.setPosition(ghost1.x, ghost1.y); // Update ghost position
+        ghost_shape2.setPosition(ghost2.x, ghost2.y); // Update ghost position
         window.draw(pacman_shape); // Draw the player (yellow circle)
-        window.draw(ghost_shape); // Draw the ghost (red circle)
+        window.draw(ghost_shape1); // Draw the ghost (red circle)
+        window.draw(ghost_shape2);
         window.display();
         usleep(150000); // Sleep for 0.3 seconds
         if(closwindow == 1)
         {
             window.close();
         }
-        //cout<<"pacman_x: "<<pacman_x<<" pacman_y: "<<pacman_y<<endl;
+        cout<<"pacman_x: "<<pacman_x<<" pacman_y: "<<pacman_y<<endl;
     }
 
     // Join threads
