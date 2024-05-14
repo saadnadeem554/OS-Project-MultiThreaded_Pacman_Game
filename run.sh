0ba4ae320fd76bb06d73166ed4d52e1ddc993a8b
atifibrahim7 <i221249@nu.edu.pk>
2024-05-14 03:48:49 -0400

    game ending + sounds

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-g++ main.cpp -o sfml -lsfml-graphics -lsfml-window -lsfml-system
+g++ main.cpp -o sfml -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 # if the first command is successful, run the executable
 if [ $? -eq 0 ]; then
     ./sfml
